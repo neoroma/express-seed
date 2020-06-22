7626484c2d24e7ad7bca786f20371e362e83ae7b
Roman Shestakov <neoroma@gmail.com>
2020-06-22 13:23:03 +0300

    Add error handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bp from 'body-parser'
+import axios from 'axios'
 
 const app = express()
 const port = process.env.PORT || 3015
@@ -9,10 +10,29 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/message', ({ body }, res) => {
   const { question } = body
+  console.log('This is the question', question)
   res.json({
     question,
     answer: '😼',
   })
+})
+
+const errorHandler = (res) => (error) => {
+  const {
+    response: { status = 400 },
+  } = error
+  res.status(status).json({ error: error.toString() })
+}
+
+app.post('/todos', async ({ body }, res) => {
+  const { id } = body
+
+  try {
+    const { data } = await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    res.json(data)
+  } catch (error) {
+    errorHandler(res)(error)
+  }
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
