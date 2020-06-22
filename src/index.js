efa3f109e35850930f3fd33e49b3e01f0525c5d1
Roman Shestakov <neoroma@gmail.com>
2020-06-22 12:55:06 +0300

    Add body parsing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import express from 'express'
+import bp from 'body-parser'
 
 const app = express()
 const port = process.env.PORT || 3015
 
+app.use(bp.json())
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-)
+app.post('/message', ({ body }, res) => {
+  const { question } = body
+  res.json({
+    question,
+    answer: '😼',
+  })
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
