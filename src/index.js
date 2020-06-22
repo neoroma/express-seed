be1ee8e28402a6dcefc59538e8b82a4275192bbc
Roman Shestakov <neoroma@gmail.com>
2020-06-22 12:46:03 +0300

    Add files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3015
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+)
